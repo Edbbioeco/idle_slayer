9b5b249c02db7d68fda5cf52099711b321d51974
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-26 23:01:21 -0300

    Criar o data frame dos valores do teste Qui-Quadrado

--- teste_de_frequencia.R
+++ teste_de_frequencia.R
@@ -40,3 +40,27 @@ freq <- dados |>
   dplyr::arrange(bau)
 
 freq
+
+## Teste Qui-Quadrado ----
+
+df_x2 <- purrr::map2_dfr(
+  freq[2:3] |>
+    names(),
+  c(6, 20),
+  \(conteudo, frequencia){
+
+    qq <- chisq.test(freq[[conteudo]])
+
+    tibble::tibble(Conteúdo = conteudo,
+                   sts = paste0("X² = ",
+                                qq$statistic |> round(2),
+                                ", df = ",
+                                qq$parameter,
+                                ", p = ",
+                                qq$p.value |> round(2)),
+                   Frequência = frequencia,
+                   Bau = 15)
+
+    })
+
+df_x2
